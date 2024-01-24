479e9495488ee3a45ea57864eb53fc7aa87388ac
wojtek239 <wojtekopelt1@wp.pl>
2024-01-24 19:23:07 +0100

    feat/exc 3 done

--- Project/app.py
+++ Project/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from views import index, add_movie, summary
 from models import init_db
+import os
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+app.config['SECRET_KEY'] = os.urandom(16).hex()
+print("CSRF Key:", app.config['SECRET_KEY'])
 
 init_db(app)
 
